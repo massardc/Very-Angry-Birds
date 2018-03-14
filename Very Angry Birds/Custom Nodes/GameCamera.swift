54f3621a73d67f9487c5ec26e46ded29f235a00c
Clement M <massardc@users.noreply.github.com>
2018-03-14 19:37:52 +0100

    Panning

---
+++ Very Angry Birds/Custom Nodes/GameCamera.swift
@@ -0,0 +1,15 @@
+//
+//  GameCamera.swift
+//  Very Angry Birds
+//
+//  Created by ClementM on 14/03/2018.
+//  Copyright © 2018 ClementM. All rights reserved.
+//
+
+import SpriteKit
+
+class GameCamera: SKCameraNode {
+
+    
+    
+}
--- Very Angry Birds/Scenes/GameScene.swift
+++ Very Angry Birds/Scenes/GameScene.swift
@@ -12,17 +12,37 @@ import GameplayKit
 class GameScene: SKScene {
     
     //MARK: - Variables
-    let gameCamera = SKCameraNode()
+    let gameCamera = GameCamera()
+    var panRecognizer = UIPanGestureRecognizer()
     
     override func didMove(to view: SKView) {
         addCamera()
+        setupGestureRecognizers()
     }
     
     //MARK: - Custom function
+    func setupGestureRecognizers() {
+        guard let view = view else { return }
+        panRecognizer = UIPanGestureRecognizer(target: self, action: #selector(pan))
+        view.addGestureRecognizer(panRecognizer)
+    }
+    
     func addCamera() {
         guard let view = view else { return }
         addChild(gameCamera)
         gameCamera.position = CGPoint(x: view.bounds.size.width / 2, y: view.bounds.size.height / 2)
         camera = gameCamera
     }
+}
+
+//MARK: - Extension for Gesture Recognizer methods
+extension GameScene {
+    
+    @objc func pan(sender: UIPanGestureRecognizer) {
+        guard let view = view else { return }
+        let translation = sender.translation(in: view)
+        gameCamera.position = CGPoint(x: gameCamera.position.x - translation.x, y: gameCamera.position.y + translation.y)
+        sender.setTranslation(CGPoint.zero, in: view)
+    }
+    
 }
